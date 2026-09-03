40496c244f82d2b66a15fde098f74cce6078fb63
Pl4zmatic <anthony.david2004@pm.me>
2026-09-03 15:32:47 +0200

    file selection and cover image preview

---
+++ scripts/events/chooseFiles.js
@@ -0,0 +1,54 @@
+import { previewListItems, addEventsToPreviewListItems } from "./selectSubfolder.js";
+
+// Read Files
+const contentEmpty = document.getElementById("contentEmpty");
+const contentSelected = document.getElementById("contentSelected");
+const files = document.getElementById("fileInput");
+const previewList = document.getElementById("previewList");
+const preview = document.getElementById("preview");
+
+files.addEventListener("change", (event) => {
+    const selectedFiles = getSelectedFilesSorted(event.target.files);
+
+    previewListItems.length = 0; // Clear the previewListItems array before adding new items
+    previewList.innerHTML = ""; // Clear the previewList container before adding new items
+    preview.innerHTML = ""; // Clear the preview container before adding new items
+
+    selectedFiles.forEach((selectedFile, index) => {
+        const image = document.createElement("img");
+        image.src = URL.createObjectURL(Array.from(selectedFile.files).find((file) => file.type.startsWith("image/")));
+        image.id = `previewImage${index}`;
+        image.dataset.subfolder = selectedFile.subfolder;
+
+        previewListItems.push(image);
+        previewList.appendChild(image);
+    });
+    addEventsToPreviewListItems();
+
+    if (!contentEmpty.classList.contains("hidden")) {
+        contentEmpty.classList.toggle("hidden");
+        contentSelected.classList.toggle("hidden");
+    }
+});
+
+function getSelectedFilesSorted(selectedFiles) {
+    const sortedFiles = [];
+    const folderNames = new Set(Array.from(selectedFiles).map((file) => file.webkitRelativePath.split("/").slice(0, 2).join("/")));
+    for (const folderName of folderNames) {
+        const folderFiles = Array.from(selectedFiles).filter((file) => file.webkitRelativePath.startsWith(folderName));
+
+        sortedFiles.push({
+            folder: folderName.split("/")[0],
+            subfolder: folderName.split("/")[1],
+            files: folderFiles,
+        });
+    }
+    console.log(`Selected Files: ${JSON.stringify(sortedFiles, null, 2)}`);
+    return sortedFiles;
+}
+
+// Invoke fileInput field by button id: otherButton
+const otherButton = document.getElementById("otherButton");
+otherButton.addEventListener("click", () => {
+    files.click();
+});
---
+++ scripts/events/selectSubfolder.js
@@ -0,0 +1,22 @@
+export let previewListItems = [];
+
+export function addEventsToPreviewListItems() {
+    for (const image of previewListItems) {
+        image.addEventListener("click", () => {
+            const preview = document.getElementById("preview");
+            const fullImage = document.createElement("img");
+            fullImage.src = image.src;
+
+            const subfolderNameH2 = document.createElement("h2");
+            subfolderNameH2.textContent = image.dataset.subfolder;
+
+            if (preview.children.length > 0) {
+                preview.replaceChild(subfolderNameH2, preview.firstChild);
+                preview.replaceChild(fullImage, preview.lastChild);
+            } else {
+                preview.appendChild(subfolderNameH2);
+                preview.appendChild(fullImage);
+            }
+        });
+    }
+}
